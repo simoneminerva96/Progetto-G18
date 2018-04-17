9164a760123710318afe36ebd233dd5e8423d844
FrancescaMarini <francesca.marini03@universitadipavia.it>
2018-04-17 16:25:44 +0200

    first commit

---
+++ Role.java
@@ -0,0 +1,10 @@
+public class Role {
+    private String Name;
+    private Sport Sport;
+
+    public Role (String Name, Sport Sport) {
+        this.Name=Name;
+        this.Sport=Sport;
+    }
+
+}
