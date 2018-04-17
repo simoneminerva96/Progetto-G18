ca8d6e5119a97c0c37a4a23a691a613b9ff6313f
FrancescaMarini <francesca.marini03@universitadipavia.it>
2018-04-17 16:31:18 +0200

    first commit

---
+++ src/Player.java
@@ -0,0 +1,14 @@
+public class Player {
+
+
+    private String Name, Surname;
+    private int Age;
+    private Sex Sex_Player;
+
+    public Player (String Name, String Surname) {
+        this.Name=Name;
+        this.Surname=Surname;
+        this.Age=Age;
+        this.Sex_Player=Sex_Player;
+    }
+}
---
+++ src/Sport.java
@@ -0,0 +1,10 @@
+public class Sport {
+    private String Name;
+    private int N_Players;
+
+    public Sport (String Name) {
+        this.Name=Name;
+        this.N_Players=N_Players;
+
+    }
+}
