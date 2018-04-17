e52a010bd7e7926aff4cdab15a97b49cbf587d04
FrancescaMarini <37289536+FrancescaMarini@users.noreply.github.com>
2018-04-17 16:35:25 +0200

    Delete Team.java

--- Team.java
+++
@@ -1,8 +0,0 @@
-public class Team {
-    private Level Level_Event;
-    private char Sex;
-    private String Age_Range;        //Reange di eta`
-    private Sport Sport;
-
-    public Team(Sport Sport, Level Level_Event, )
-}
