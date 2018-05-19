10b803f654047bca48fcc239b54be8c1831a0ddc
andreacarra <37333225+andreacarra@users.noreply.github.com>
2018-05-19 12:05:33 +0200

    Updating class Team and Player

---
+++ src/Player.java
@@ -0,0 +1,12 @@
+public class Player {
+    private String name, surname;
+    private int age;
+    private Sex sexPlayer;
+
+    public Player (String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.sexPlayer = sexPlayer;
+    }
+}
