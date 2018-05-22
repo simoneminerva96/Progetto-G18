6f71c456281c63d8f98f3cb65b4a45f6b0889b9c
andreacarra <37333225+andreacarra@users.noreply.github.com>
2018-05-22 21:47:45 +0200

    Update class Role.java

---
+++ src/Role.java
@@ -0,0 +1,10 @@
+
+public class Role {
+    private String name;
+    private SportType sport;
+
+    public Role (String name, SportType sport) {
+        this.name=name;
+        this.sport=sport;
+    }
+}
