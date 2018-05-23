d4e289166e212d6db1d6e2b551737cfbcf54091b
andreacarra <37333225+andreacarra@users.noreply.github.com>
2018-05-23 17:49:24 +0200

    Sign in, log in, and select_event method

--- src/Users.java
+++ src/Users.java
@@ -1,4 +1,3 @@
-import java.util.Date;
 import java.sql.*;
 
 import static java.sql.DriverManager.getConnection;
@@ -8,46 +7,116 @@ public class Users {
     private String password;
     private String nameP;
     private String surname;
-    private String sex;
+    private Sex sex;
     private int age;
+    private boolean loggedIn;
 
-    public Users (String userName, String password, String nameP, String surname, String sex, int age){
+    public Users (String userName, String password, String nameP, String surname, Sex sex, int age){
         this.userName = userName;
         this.password = password;
         this.nameP = nameP;
         this.surname = surname;
         this.sex = sex;
         this.age = age;
+        loggedIn = false;
     }
 
-    public Boolean addUsers() throws ClassNotFoundException, SQLException {
-        String query = "{ ?=call add-user(?,?,?,?,?,?)}";
-        ResultSet res;
-        Boolean r = null;
+    public Sex getSex() {
+        return sex;
+    }
+
+    public boolean signUp() throws ClassNotFoundException, SQLException {
+
+        boolean r = false;
+        Connection con = null;
         try {
             String connectionString = "jdbc:mysql://localhost:3306/selection?user=root&password=qazxswedc4321@";
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con = getConnection(connectionString);
-            CallableStatement stmt = con.prepareCall(query);
-            stmt.registerOutParameter(1, Types.VARCHAR);
-            stmt.setString(2, userName);
-            stmt.setString(3, password);
-            stmt.setString(4, nameP);
-            stmt.setString(5, surname);
-            stmt.setString(6, sex);
-            stmt.setInt(7, age);
-
-            res = stmt.executeQuery();
-
-            while (res.next()) {
-                r = res.getBoolean(1); //Il risultato viene inserito in una stringa
+            con = getConnection(connectionString);
+            Statement stmt = con.createStatement();
+            Statement stm = con.createStatement();
+            ResultSet res = stm.executeQuery("SELECT * FROM USERS WHERE USERNAME = '"+userName+"'");
+            if (res.next()) {
+                System.out.println("User already exists");
+            }
+            else {
+                int count = stmt.executeUpdate("INSERT INTO USERS (USERNAME, PASSWORD_P, NAME_PLAYER, SURNAME_PLAYER, SEX, AGE) VALUES ('"+userName+"','"+password+"', '"+nameP+"', '"+surname+"', '"+sex+"', '"+age+"')");
+                if (count == 0) {
+                    r = false;
+                }
+                else {
+                    r = true;
+                    System.out.println("User registered");
+                }
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
 
+        con.close();
+
         return r;
 
     }
 
+    public boolean logIn(String userName, String password) throws ClassNotFoundException, SQLException {
+        Connection con = null;
+
+        try {
+            String connectionString = "jdbc:mysql://localhost:3306/selection?user=root&password=qazxswedc4321@";
+            Class.forName("com.mysql.jdbc.Driver");
+            con = getConnection(connectionString);
+            Statement stm = con.createStatement();
+            ResultSet res = stm.executeQuery("SELECT * FROM USERS WHERE USERNAME = '"+this.userName+"'");
+
+            //System.out.println(res.getString("PASSWORD_P"));
+            if (res.next()) {
+                Statement stm1 = con.createStatement();
+                if (res.getString("PASSWORD_P").equals(password)) {
+                    System.out.println("Login succeffull");
+                    loggedIn = true;
+                }
+                else {
+                    System.out.println("Incorrect username or password");
+                }
+            }
+            else {
+                System.out.println("User does not exists");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        con.close();
+        return loggedIn;
+    }
+
+    public boolean showEvents (SportType sport, Level level) throws ClassNotFoundException, SQLException {
+        Connection con = null;
+
+        boolean r = false;
+
+        if (loggedIn == true) {
+            try {
+                String connectionString = "jdbc:mysql://localhost:3306/selection?user=root&password=qazxswedc4321@";
+                Class.forName("com.mysql.jdbc.Driver");
+                con = getConnection(connectionString);
+                Statement stm = con.createStatement();
+                ResultSet res = stm.executeQuery("SELECT * FROM ADVERTISMENT WHERE (SPORT = '"+sport+"' OR LEVEL_EVENT = '"+level+"')");
+                if (res.next()) {
+                    while (res.next()) {
+                        System.out.println(res.getString("LOCATION") + " " + res.getString("EVENT_DATE") + " " + res.getString("EVENT_HOUR"));
+                        r = true;
+                    }
+                } else {
+                    System.out.println("Event not found");
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+            con.close();
+        } else {
+            System.out.println("User not loggedin");
+        }
+        return r;
+    }
 }
