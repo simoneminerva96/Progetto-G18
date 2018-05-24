1f8784506b203ea3dcbeb2ce1e1ba6d02c4f30b7
andreacarra <37333225+andreacarra@users.noreply.github.com>
2018-05-24 23:32:13 +0200

    Packaging advertisment and related methods

---
+++ src/advertisment/Advertisment.java
@@ -0,0 +1,4 @@
+package advertisment;
+
+public class Advertisment {
+}
---
+++ src/advertisment/Location.java
@@ -0,0 +1,21 @@
+package advertisment;
+
+public class Location {
+    private String region;
+    private String city;
+    private String province;
+    private String address;
+
+    public Location(String city, String address, String region, String province) {
+        this.city = city;
+        this.address = address;
+        this.region = region;
+        this.province = province;
+    }
+
+
+    @Override
+    public String toString() {
+        return city + " " + address + " " + region + " " + province;
+    }
+}
