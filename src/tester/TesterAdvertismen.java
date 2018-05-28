1fc0d027d097914deb39d27ad1c1e43cdc985b17
andreacarra <37333225+andreacarra@users.noreply.github.com>
2018-05-28 18:03:29 +0200

    Update

--- src/tester/TesterAdvertismen.java
+++ src/tester/TesterAdvertismen.java
@@ -16,10 +16,10 @@ import static java.util.Calendar.SEPTEMBER;
 
 public class TesterAdvertismen {
     public static void main (String[] args) {
-        java.util.Date d = new java.util.Date(2018, 9, 12);
-        Time t = new Time(18,00,00);
+
+
         Location l1 = new Location("STRADELLA", "VIA G. BOVIO 68", "LOMBARDIA", "PV");
-        Advertisment a1 = new Advertisment(SportType.CALCETTO, l1, d, t, Role.GOALKEEPER, 18, 21, Level.HIGH, Sex.M );
+        Advertisment a1 = new Advertisment(SportType.CALCETTO, l1, "2017-09-12", "18:00:00", Role.PORTIERE, 18, 21, Level.HIGH, Sex.M );
 
         a1.checkDate();
 
