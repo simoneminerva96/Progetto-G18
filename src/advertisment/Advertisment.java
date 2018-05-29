b5d6ee20b94a70cad4d609bc69730c105575b7e1
andreacarra <37333225+andreacarra@users.noreply.github.com>
2018-05-29 15:31:08 +0200

    Add periodicity

--- src/advertisment/Advertisment.java
+++ src/advertisment/Advertisment.java
@@ -14,17 +14,19 @@ public class Advertisment {
     private Level level;
     private Location location;
     private Role role;
+    private Periodicity periodicity;
     private String date;
     private String time;
     private int ageMin;
     private int ageMax;
 
-    public Advertisment (SportType sport, Location location, String date, String time, Role role, int ageMin, int ageMax, Level level, Sex sex) {
+    public Advertisment (SportType sport, Location location, String date, String time, Role role, Periodicity periodicity, int ageMin, int ageMax, Level level, Sex sex) {
         this.sport = sport;
         this.location = location;
         this.date = date;
         this.time = time;
         this.role = role;
+        this.periodicity= periodicity;
         this.ageMin = ageMin;
         this.ageMax = ageMax;
         this.level = level;
@@ -128,6 +130,14 @@ public class Advertisment {
 
     public Sex getSex() {
         return sex;
+    }
+
+
+
+
+
+    public  Periodicity getPeriodicity() {
+        return periodicity;
     }
 
 
---
+++ src/advertisment/Periodicity.java
@@ -0,0 +1,5 @@
+package advertisment;
+
+public enum Periodicity {
+    NONE, GIORNLIERA, SETTIMANALE, MENSILE;
+}
