6e605c4a6a8127bc887333e9f1cbd9e44682343f
andreacarra <37333225+andreacarra@users.noreply.github.com>
2018-06-08 10:28:06 +0200

    Testing getUser method

--- src/tester/TesterAddUsers.java
+++ src/tester/TesterAddUsers.java
@@ -10,13 +10,13 @@ public class TesterAddUsers {
     public static void main (String [] args) throws java.lang.ClassNotFoundException, SQLException {
 
         // db_connector con = new db_connector();
-        Users u = new Users("ANDRE", "111", "ANDREA", "CARRA", Sex.M, 21);
-        Users u1 = new Users("MARA", "222", "MARA", "GARZIA", Sex.F, 41);
-        Users u2 = new Users("DORIANO", "323", "DORIANO", "CARRA", Sex.M, 61);
-        Users u3 = new Users("SIMO", "123", "SIMONE", "COSI", Sex.M, 28);
-        Users u4 = new Users("IL_BUSSI", "Migliore", "MARCO", "TAMBUSSI", Sex.M, 21);
-        Users u5 = new Users("ze", "321", "ALESSANDRO", "ZERBA", Sex.M, 22);
-        Users u6 = new Users("MIRQO", "1997", "MIRKO","DE SORICELLIS", Sex.M, 21);
+        User u = new User("ANDRE", "111", "ANDREA", "CARRA", Sex.M, 21);
+        User u1 = new User("MARA", "222", "MARA", "GARZIA", Sex.F, 41);
+        User u2 = new User("DORIANO", "323", "DORIANO", "CARRA", Sex.M, 61);
+        User u3 = new User("SIMO", "123", "SIMONE", "COSI", Sex.M, 28);
+        User u4 = new User("IL_BUSSI", "Migliore", "MARCO", "TAMBUSSI", Sex.M, 21);
+        User u5 = new User("ze", "321", "ALESSANDRO", "ZERBA", Sex.M, 22);
+        User u6 = new User("MIRQO", "1997", "MIRKO","DE SORICELLIS", Sex.M, 21);
         //u3.signUp();
         //u4.signUp();
         //u5.signUp();
@@ -29,6 +29,8 @@ public class TesterAddUsers {
         Advertisment a2 = new Advertisment(SportType.TENNIS, l1, "2018-06-08", "17:00:00", Role.NONE, Periodicity.SETTIMANALE, 18, 21, Level.HIGH, Sex.F);
         //u6.addEvent(a1);
 
+
+        u6.getUser();
         u6.showEvents(SportType.CALCETTO, Level.HIGH, Periodicity.GIORNLIERA);
         //u6.addEvent(a2);
         //u3.logIn("SIMO", "123");
