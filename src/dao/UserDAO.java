b4f4132eda9a9c927c65ae527e558dbdcbb1dc14
andreacarra <37333225+andreacarra@users.noreply.github.com>
2018-06-08 10:26:58 +0200

    getUser method

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -6,6 +6,7 @@ import java.sql.*;
 import java.util.ArrayList;
 
 import static java.sql.DriverManager.getConnection;
+import static java.sql.DriverManager.println;
 
 public class UserDAO {
 
@@ -122,6 +123,46 @@ public class UserDAO {
     }
 
 
+    public static User getUser(String userName) {
+        Connection con = null;
 
+        String password = null;
+        String name = null;
+        String surname = null;
+        String sex1 = null;
+        Sex sex = null;
+        int age = 0;
+
+        try {
+            String connectionString = "jdbc:mysql://localhost:3306/selection?user=root&password=qazxswedc4321@";
+            Class.forName("com.mysql.jdbc.Driver");
+            con = getConnection(connectionString);
+            Statement stm = con.createStatement();
+            ResultSet res = stm.executeQuery("SELECT * FROM USERS WHERE USERNAME = '"+userName+"'");
+            res.next();
+
+            password = res.getString("PASSWORD_P");
+            name = res.getString("NAME_PLAYER");
+            surname = res.getString("SURNAME_PLAYER");
+            sex1 = res.getString("SEX");
+            if (sex1.equals("M")) {
+                sex = Sex.M;
+            }
+            else if (sex1.equals("F")) {
+                sex = Sex.F;
+            }
+            age = res.getInt("AGE");
+
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } catch (ClassNotFoundException c) {
+            System.out.println(c.getMessage());
+        }
+
+
+        User u = new User(userName, password, name, surname, sex, age);
+        return u;
+    }
 
 }
