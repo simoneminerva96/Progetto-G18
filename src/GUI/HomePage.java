f2dbd2ddebb76a20fd5af1b367a0e680d2f7c51f
Mattia-95 <37336444+Mattia-95@users.noreply.github.com>
2018-06-16 10:47:10 +0200

    Add files via upload

--- src/GUI/HomePage.java
+++ src/GUI/HomePage.java
@@ -1,12 +1,14 @@
 package GUI;
 
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import Controller.AdvertisementController;
+import advertisment.Advertisment;
+import advertisment.Periodicity;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
@@ -17,6 +19,8 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import user.Level;
+import user.SportType;
 
 
 public class HomePage {
@@ -50,8 +54,11 @@ public class HomePage {
 
 
         Label sportLabel= new Label("Sport:");
-        ObservableList <String> sportList= FXCollections.observableArrayList(
-                "Calcetto","Pallavolo","Basket","Tennis"
+        ObservableList <SportType> sportList= FXCollections.observableArrayList(
+                SportType.CALCETTO,
+                SportType.PALLAVOLO,
+                SportType.TENNIS,
+                SportType.BASKET
         );
         ComboBox comboBoxSport= new ComboBox(sportList);
         comboBoxSport.setPromptText("Choose sport");
@@ -59,193 +66,45 @@ public class HomePage {
 
 
         Label levelLabel = new Label("Level:");
-        ObservableList<String>level= FXCollections.observableArrayList(
-                "Low",
-                "Medium",
-                "High"
+        ObservableList<Level>level= FXCollections.observableArrayList(
+                Level.LOW,
+                Level.MEDIUM,
+                Level.HIGH
+
         );
         ComboBox comboBoxLevel= new ComboBox(level);
         comboBoxLevel.setPromptText("Choose level");
         GridPane.setConstraints(comboBoxLevel, 0,5);
 
         //Periodicity Label
-        Label periodicyLabel= new Label("Periodicità:");
+        Label periodicityLabel= new Label("Periodicity:");
 
-        //check box Giornaliero
-        CheckBox checkBoxGiornaliero = new CheckBox("Giornaliero");
+        ObservableList <Periodicity> period = FXCollections.observableArrayList(
+                Periodicity.GIORNALIERA,
+                Periodicity.SETTIMANALE,
+                Periodicity.MENSILE,
+                Periodicity.NONE
+        );
+        ComboBox comboBoxPeriodicity = new ComboBox(period);
+        comboBoxPeriodicity.setPromptText("Choose periodicity");
 
+        ListView <String> advArea = new ListView<>();
+        ObservableList<String> viewAdvs =FXCollections.observableArrayList (
+        );
+        advArea.setItems(viewAdvs);
 
-        //Check box Settimanale
-        CheckBox checkBoxSettimanale= new CheckBox("Settimanale");
-
-
-        //Check box Mensile
-        CheckBox checkBoxMensile = new CheckBox("Mensile");
-
-        Label locationLabel = new Label("Location:");
-        ObservableList<String>location= FXCollections.observableArrayList(
-                "_",
-                "Abruzzo",
-                "Basilicata",
-                "Calabria",
-                "Campania",
-                "Emilia-Romagna",
-                "Friuli-Venezia-Giulia",
-                "Lazio",
-                "Liguria",
-                "Lombardia",
-                "Marche",
-                "Piemonte",
-                "Puglia",
-                "Sardegna",
-                "Sicilia",
-                "Toscana",
-                "Trentino-Alto Adige",
-                "Umbria",
-                "Valle d'Aosta",
-                "Veneto"
-        );
-
-        ComboBox comboBoxLocation= new ComboBox(location);
-        comboBoxLocation.setPromptText("Choose region");
-        GridPane.setConstraints(comboBoxLocation, 1,6);
-
-
-
-        ObservableList AbruzzoList = FXCollections.observableArrayList(
-                "AQ","CH","PE","TE"
-        );
-        ObservableList BasilicataList = FXCollections.observableArrayList(
-                "MT", "PZ"
-        );
-        ObservableList CalabriaList = FXCollections.observableArrayList(
-                "CZ","CS","KR","RC","VV"
-        );
-        ObservableList CampaniaList = FXCollections.observableArrayList(
-                "AV","BN","CE","NA","SA"
-        );
-        ObservableList EmiliaRomagnaList = FXCollections.observableArrayList(
-                "BO","FE","FC","MO","PR","PC","RA","RE","RN"
-        );
-        ObservableList FriuliList = FXCollections.observableArrayList(
-                "GO","PN","TS","UD"
-        );
-        ObservableList LazioList = FXCollections.observableArrayList(
-                "FR","LT","RI","RM","VT"
-        );
-        ObservableList LiguriaList = FXCollections.observableArrayList(
-                "GE","IM","SP","SV"
-        );
-        ObservableList LombardiaList = FXCollections.observableArrayList(
-                "BG","BS","CO","CR","LC","LO","MN","MI","MB","PV","SO","VA"
-        );
-        ObservableList MarcheList = FXCollections.observableArrayList(
-                "AN","AP","FM","MC","PU"
-        );
-        ObservableList PiemonteList = FXCollections.observableArrayList(
-                "AL","AT","BI","CN","NO","TO","VB","VC"
-        );
-        ObservableList PugliaList = FXCollections.observableArrayList(
-                "BA","BT","BR","FG","LE","TA"
-        );
-        ObservableList SardegnaList = FXCollections.observableArrayList(
-                "CA","CI","VS","NU","OG","OT","OR","SS"
-        );
-        ObservableList SiciliaList = FXCollections.observableArrayList(
-                "AG","CL","CT","EN","ME","PA","RG","SR","TP"
-        );
-        ObservableList ToscanaList = FXCollections.observableArrayList(
-                "AR","FI","GR","LI","LU","MS","PI","PT","PO","SI"
-        );
-        ObservableList TrentinoList = FXCollections.observableArrayList(
-                "BZ","TN"
-        );
-        ObservableList UmbriaList = FXCollections.observableArrayList(
-                "PG","TR"
-        );
-        ObservableList ValledaostaList = FXCollections.observableArrayList(
-                "AO"
-        );
-        ObservableList VenetoList = FXCollections.observableArrayList(
-                "BL","PD","RO","TV","VE","VR","VI"
-        );
-
-
-        final ComboBox comboBoxProvince= new ComboBox();
-        GridPane.setConstraints(comboBoxProvince,2,6);
-
-        comboBoxLocation.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
-            @Override
-            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
-                switch (newValue.toString()){
-                    case "Abruzzo":
-                        comboBoxProvince.setItems(AbruzzoList);
-                        break;
-                    case "Basilicata":
-                        comboBoxProvince.setItems(BasilicataList);
-                        break;
-                    case "Calabria":
-                        comboBoxProvince.setItems(CalabriaList);
-                        break;
-                    case "Campania":
-                        comboBoxProvince.setItems(CampaniaList);
-                        break;
-                    case "Emilia-Romagna":
-                        comboBoxProvince.setItems(EmiliaRomagnaList);
-                        break;
-                    case "Friuli-Venezia-Giulia":
-                        comboBoxProvince.setItems(FriuliList);
-                        break;
-                    case "Lazio":
-                        comboBoxProvince.setItems(LazioList);
-                        break;
-                    case "Liguria":
-                        comboBoxProvince.setItems(LiguriaList);
-                        break;
-                    case "Lombardia":
-                        comboBoxProvince.setItems(LombardiaList);
-                        break;
-                    case "Marche":
-                        comboBoxProvince.setItems(MarcheList);
-                        break;
-                    case "Piemonte":
-                        comboBoxProvince.setItems(PiemonteList);
-                        break;
-                    case "Puglia":
-                        comboBoxProvince.setItems(PugliaList);
-                        break;
-                    case "Sardegna":
-                        comboBoxProvince.setItems(SardegnaList);
-                        break;
-                    case "Sicilia":
-                        comboBoxProvince.setItems(SiciliaList);
-                        break;
-                    case "Toscana":
-                        comboBoxProvince.setItems(ToscanaList);
-                        break;
-                    case "Trentino-Alto Adige":
-                        comboBoxProvince.setItems(TrentinoList);
-                        break;
-                    case "Umbria":
-                        comboBoxProvince.setItems(UmbriaList);
-                        break;
-                    case "Valle d'Aosta":
-                        comboBoxProvince.setItems(ValledaostaList);
-                        break;
-                    case "Veneto":
-                        comboBoxProvince.setItems(VenetoList);
-                        break;
-                }
-            }
-        });
-        comboBoxProvince.setPromptText("Choose province");
 
 
         Button searchButton= new Button("Search");
+        searchButton.setOnAction(event -> {
+            AdvertisementController advertisementController = new AdvertisementController();
+            advertisementController.ShowAdvertisments((SportType) comboBoxSport.getValue(), (user.Level) comboBoxLevel.getValue(), (Periodicity) comboBoxPeriodicity.getValue());
+        });
+
 
         Button newAdvertisement = new Button("New Advertisement");
         newAdvertisement.setOnAction(event -> NewAdvertisement.display("New Advertisement"));
-        newAdvertisement.setPrefSize(100,20);
+        newAdvertisement.setPrefSize(150,20);
 
         Button profileButton = new Button("Profile");
         profileButton.setOnAction(event -> {
@@ -263,12 +122,10 @@ public class HomePage {
         logOut.setPrefSize(100,20);
 
 
-        ListView advArea = new ListView();
 
 
         hbox.getChildren().addAll(newAdvertisement,profileButton,logOut);
-        periodicity.getChildren().addAll(checkBoxGiornaliero,checkBoxSettimanale,checkBoxMensile);
-        vBox.getChildren().addAll(text,sportLabel,comboBoxSport,levelLabel,comboBoxLevel,locationLabel,comboBoxLocation, comboBoxProvince, periodicyLabel,periodicity,searchButton);
+        vBox.getChildren().addAll(text,sportLabel,comboBoxSport,levelLabel,comboBoxLevel, periodicityLabel, comboBoxPeriodicity,searchButton);
 
         BorderPane borderPane= new BorderPane();
         borderPane.setTop(hbox);
