f5d937def99b464a0c8a39028bbb7643214adcc0
Mattia-95 <37336444+Mattia-95@users.noreply.github.com>
2018-06-16 10:43:00 +0200

    Add files via upload

--- src/Controller/AdvertisementController.java
+++ src/Controller/AdvertisementController.java
@@ -1,9 +1,40 @@
 package Controller;
 
 
+import advertisment.Advertisment;
+import advertisment.Location;
+import advertisment.Periodicity;
+import advertisment.Role;
+import user.Level;
+import user.Sex;
+import user.SportType;
 
 
 public class AdvertisementController {
+    public SportType sport;
+    public Sex sex;
+    public Level level;
+    public Location location;
+    public Role role;
+    public Periodicity periodicity;
+    public String date;
+    public String time;
+    public int ageMin;
+    public int ageMax;
+
+    public Advertisment advertisment = new Advertisment(sport, location, date, time, role, periodicity, ageMin, ageMax, level, sex);
+
+    public Advertisment AddEvent(SportType sport1, Location location1, String date1, String time1, Role role1, Periodicity periodicity1, int ageMin1, int ageMax1, Level level1, Sex sex1){
+        advertisment = new Advertisment(sport1, location1, date1, time1, role1, periodicity1, ageMin1, ageMax1, level1, sex1);
+        advertisment.addEvent(advertisment);
+        return advertisment;
+    }
+
+    public static void ShowAdvertisments(SportType sport, Level level, Periodicity periodicity){
+        Advertisment.showEvents(sport, level, periodicity);
+
+    }
+
 
 
 }
