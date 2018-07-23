82bb44af4628bc5f92a4657ee1a7c30958af89d0
Mattia-95 <37336444+Mattia-95@users.noreply.github.com>
2018-07-23 16:38:47 +0200

    Add files via upload

--- src/GUI/HomePage.java
+++ src/GUI/HomePage.java
@@ -2,17 +2,19 @@ package GUI;
 
 
 import Controller.AdvertisementController;
-import advertisment.Advertisment;
 import advertisment.Periodicity;
 import dao.AdvertismentDAO;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -22,7 +24,7 @@ import javafx.stage.Stage;
 import user.Level;
 import user.SportType;
 
-import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 
 public class HomePage {
@@ -38,7 +40,7 @@ public class HomePage {
         HBox hbox = new HBox();
         hbox.setPadding(new Insets(15, 12, 15, 12));
         hbox.setSpacing(10);
-        hbox.setStyle("-fx-background-color: Blue;");
+        hbox.setStyle("-fx-background-color: LightBlue;");
 
         HBox hhbox= new HBox();
         hhbox.setPadding(new Insets(15, 12, 15, 12));
@@ -103,16 +105,43 @@ public class HomePage {
             list.setItems(viewAdvs);
         });
 
-
-/*
-
-
-        if (me.getClickCount() == 2) {
-            System.out.println("OK");
-        }
+        list.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                        if(event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 2) {
 
 
-*/
+                              String str = event.getTarget().toString();
+
+                              StringTokenizer strTok = new StringTokenizer(str,"\"");
+                              System.out.println(str);
+
+                              strTok.nextToken();
+
+                              String str2 = strTok.nextToken();
+
+                              StringTokenizer strTok2 = new StringTokenizer(str2, ":");
+
+                              String tok01 = strTok2.nextToken();
+
+                              int cod = Integer.parseInt(tok01);
+
+
+                              AdvertisementController.SelectEvent(cod);
+
+                              AdvBooking.display("Advertisement booking");
+
+                              window.close();
+
+
+                        }
+                }
+        });
+
+
+
+
+
 
         Button newAdvertisement = new Button("New Advertisement");
         newAdvertisement.setOnAction(event -> NewAdvertisement.display("New Advertisement"));
